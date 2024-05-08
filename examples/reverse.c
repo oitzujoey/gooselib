1f402c77bd39c5a29341ece26c1af0ba245fed33
Joey Herguth <oitzujoey@gmail.com>
2024-05-07 17:35:46 -0700

    Make "reverse.c" use the arena allocator.

--- examples/reverse.c
+++ examples/reverse.c
@@ -1,5 +1,6 @@
 #include "../gooselib/gooselib_string.h"
 #include "../gooselib/gooselib_array.h"
+#include "../gooselib/gooselib_allocator.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -14,21 +15,32 @@ void callback_free(void *context, void *memory) {
 }
 
 int main(int argc, char *argv[]) {
-	gooselib_array_t args = gooselib_array_create(sizeof(gooselib_dstring_t), callback_malloc, callback_free, NULL);
-	for (int index = 1; index < argc; index++) {
-		gooselib_dstring_t arg = gooselib_dstring_copyReallocateC(argv[index], callback_malloc, NULL);
-		int e = gooselib_array_pushElement(&args, &arg);
-		if (e) {
-			printf("Push failed. (%i)\n", e);
-			return 1;
+	int e = 0;
+	gooselib_allocator_t a = gooselib_allocator_create(callback_malloc, callback_free, NULL);
+	do {
+		gooselib_array_t args = gooselib_array_create(sizeof(gooselib_dstring_t),
+		                                              (gooselib_malloc_t) gooselib_alloc,
+		                                              NULL,
+		                                              &a);
+		for (int index = 1; index < argc; index++) {
+			gooselib_dstring_t arg = gooselib_dstring_copyReallocateC(argv[index], (gooselib_malloc_t) gooselib_alloc, &a);
+			e = gooselib_array_pushElement(&args, &arg);
+			if (e) {
+				printf("Push failed. (%i)\n", e);
+				e = 1;
+				break;
+			}
 		}
-	}
-	// decrease length by 1
-	gooselib_dstring_t space = gooselib_dstring_copyReallocateC(" ", callback_malloc, NULL);
-	gooselib_dstring_t result = gooselib_dstring_reverse(gooselib_dstring_join(space, args));
-	if (result.invalid) {
-		puts("Invalid string.");
-		return 1;
-	}
-	puts((const char *) result.bytes);
+		// decrease length by 1
+		gooselib_dstring_t space = gooselib_dstring_copyReallocateC(" ", (gooselib_malloc_t) gooselib_alloc, &a);
+		gooselib_dstring_t result = gooselib_dstring_reverse(gooselib_dstring_join(space, args));
+		if (result.invalid) {
+			puts("Invalid string.");
+			e = 1;
+			break;
+		}
+		puts((const char *) result.bytes);
+	} while (0);
+	(void) gooselib_free(&a);
+	return e;
 }
